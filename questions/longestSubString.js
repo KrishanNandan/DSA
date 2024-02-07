263c3c33a1e4947766798bcbb475263a0e8e4bac
KrishanNandan <krishannandan09@gmail.com>
2024-02-07 10:20:26 +0530

    change for adding another question

--- questions/longestSubString.js
+++ questions/longestSubString.js
@@ -2,7 +2,7 @@ const longestSubString = (str, subStr) => {
   let end = subStr.length;
   let count = 0;
   for (let i = 0; i < str.length; i++) {
-    let word = str.substring(i, i + end);
+    let word = str.subString(i, i + end);
     if (word === subStr) {
       count++;
     }
