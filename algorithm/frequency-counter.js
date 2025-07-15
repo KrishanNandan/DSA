ba4fce5e18b1b641889bdd732b25fb56ffabeebd
krishan nandan sharma <krishannandan09@gmail.com>
2025-07-16 00:51:27 +0530

    changes for new questions

--- algorithm/frequency-counter.js
+++ algorithm/frequency-counter.js
@@ -17,12 +17,25 @@ console.log(
 /**Note: When we put numbers as key in object then it gets sorted by default in ascending order**/
 
 
-function  sortByFrequency (str) {
-  let frequencyChars = {};
-  str.split("").forEach((val)=>{
-    frequencyChars[val]= frequencyChars[val] ? frequencyChars[val] + 1 : 1;
-  });
-  console.log(frequencyChars);
+/** Sort letters of string in descending order by number of times they occur also if occurence is same then give precendencee to their
+ * occurence in alphabet e.g "eleven" will return "eeelnv"
+ */
+function sortByFrequency(str) {
+    let frequencyChars = {};
+    str.split("").forEach((val) => {
+        frequencyChars[val] = frequencyChars[val] ? frequencyChars[val] + 1 : 1;
+    });
+    frequencyChars = Object.fromEntries([...Object.entries(frequencyChars)].sort((a, b) => {
+        if (b[1] !== a[1]) {
+            return b[1] - a[1]
+        }
+        return a[0].charCodeAt(0) - b[0].charCodeAt(0);
+    }
+    ));
+    let sortedStr = "";
+    for (let x in frequencyChars) {
+        sortedStr = sortedStr.concat(x.repeat(Number(frequencyChars[x])))
+    }
+    return sortedStr;
 }
-
-console.log(sortByFrequency("tree"));
+console.log(sortByFrequency("eleven"));
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-// function memoize(fn) {
-//     const calledMethod={};
-//     return function(...args) {
-//         const key= `${fn.name}${JSON.stringify(args)}`;
-//         if(key in calledMethod){
-//             return calledMethod[key];
-//         }
-//         else{
-//           calledMethod[key]= fn(...args);
-//            return calledMethod[key];
-//         }
-//     }
-// }
-
-function  sortByFrequency (str) {
-  let frequencyChars = {};
-  str.split("").forEach((val)=>{
-    frequencyChars[val]= frequencyChars[val] ? frequencyChars[val] + 1 : 1;
-  });
-  console.log(frequencyChars);
+function memoize(fn) {
+    const calledMethod={};
+    return function(...args) {
+        const key= `${fn.name}${JSON.stringify(args)}`;
+        if(key in calledMethod){
+            return calledMethod[key];
+        }
+        else{
+          calledMethod[key]= fn(...args);
+           return calledMethod[key];
+        }
+    }
 }
-
-console.log(sortByFrequency("tree"));
